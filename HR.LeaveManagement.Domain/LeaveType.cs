dcac73d3c0fcc15eaf40f51985e4a0fdb303f37f
rana-621 <ranaebrahim621@gmail.com>
2025-06-29 00:03:35 +0300

    Setup Domain project

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Domain;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string DefaultDays { get; set; }
+    public DateTime DateCreated { get; set; }
+}
