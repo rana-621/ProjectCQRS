06c3087c95587ea1ba66f9ccd0ff101aec7e87ba
rana-621 <ranaebrahim621@gmail.com>
2025-07-05 19:28:11 +0300

    LeaveTypeCommands

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Application.DTOs.LeaveRequest;
+
+public class CreateLeaveRequestDto
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int LeaveTypeId { get; set; }
+    public string RequestComments { get; set; } = string.Empty;
+}
