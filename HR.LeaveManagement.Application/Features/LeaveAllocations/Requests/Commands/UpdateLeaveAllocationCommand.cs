6d9e2d0dc6133595cace57223501b6cc2796f409
rana-621 <ranaebrahim621@gmail.com>
2025-07-06 15:03:58 +0300

    UpdateLeaveAllocationCommandHandler implmentation

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Commands/UpdateLeaveAllocationCommand.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
+
+public class UpdateLeaveAllocationCommand : IRequest<Unit>
+{
+    public LeaveAllocationDto LeaveAllocationDto { get; set; } = new();
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/UpdateLeaveTypeCommand.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+
+public class UpdateLeaveTypeCommand : IRequest<Unit>
+{
+    public LeaveTypeDto LeaveTypeDto { get; set; } = new();
+}
