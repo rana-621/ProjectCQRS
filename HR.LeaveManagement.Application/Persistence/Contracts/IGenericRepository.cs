4c13ff926a7c45e0df6573cc1a4a76cfe012c7b8
rana-621 <ranaebrahim621@gmail.com>
2025-07-11 15:37:11 +0300

    Implementation for GenericRepository done

--- HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -6,6 +6,6 @@ public interface IGenericRepository<T> where T : class
     Task<IReadOnlyList<T>> GetAll();
     Task<bool> Exists(int id);
     Task<T> Add(T entity);
-    Task<T> Update(T entity);
-    Task<T> Delete(int id);
+    Task Update(T entity);
+    Task Delete(T entity);
 }
