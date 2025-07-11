7dffea8cf4915f4636da53bebcfdb6779c074b46
rana-621 <ranaebrahim621@gmail.com>
2025-07-11 22:11:08 +0300

    updated...

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,8 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Persistence.Repositories;
+
+public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+{
+}
---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,29 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Persistence.Repositories;
+
+public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+{
+    private readonly HrLeaveManagementDbContext _dbContext;
+
+    public LeaveRequestRepository(HrLeaveManagementDbContext dbContext) : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? ApprovalStatus)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LeaveRequest> GetLeaveRequestsWithDetails()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
