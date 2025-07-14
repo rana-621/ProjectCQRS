d873e0df913cec377d1000e7cf3e991702eab3e9
rana-621 <ranaebrahim621@gmail.com>
2025-07-14 23:52:31 +0300

    update..

--- HR.LeaveManagement.Infrastructure/Mail/EmailSender.cs
+++ HR.LeaveManagement.Infrastructure/Mail/EmailSender.cs
@@ -1,12 +1,35 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Infrastructure;
 using HR.LeaveManagement.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace HR.LeaveManagement.Infrastructure.Mail;
 
 public class EmailSender : IEmailSender
 {
-    public Task<bool> SendEmail(Email eamil)
+    private EamilSettings _emailSettings { get; }
+
+    public EmailSender(IOptions<EamilSettings> emailSettings)
     {
-        throw new NotImplementedException();
+        _emailSettings = emailSettings.Value;
+    }
+
+    public async Task<bool> SendEmail(Email email)
+    {
+        var client = new SendGridClient(_emailSettings.ApiKey);
+        var to = new EmailAddress(email.To);
+        var from = new EmailAddress
+        {
+            Email = _emailSettings.FromAddress,
+            Name = _emailSettings.FromName
+        };
+
+        var message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+        var response = await client.SendEmailAsync(message);
+
+        return response.StatusCode == System.Net.HttpStatusCode.OK ||
+               response.StatusCode == System.Net.HttpStatusCode.Accepted;
+
     }
 }
