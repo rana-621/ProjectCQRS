5f52f63d78d40533508fb2f4772d71505efd922e
rana-621 <ranaebrahim621@gmail.com>
2025-10-16 23:44:39 +0300

    add controller

---
+++ HR.LeaveManagement.API/Controllers/LeaveRequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.API.Controllers
+{
+    public class LeaveRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
